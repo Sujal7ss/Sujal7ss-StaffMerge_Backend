8bc1834d9ef9071f790c9e3c6627310bff198aac
Sujal7ss <sujalchahande3@gmail.com>
2024-06-10 16:25:04 +0530

    cors

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
-import candidate from "./routes/candidate.js"
+import candidate from "./routes/candidate.js";
+import cors from "cors";
 const app = express();
 
-const DB="mongodb+srv://sujalchahande3:Smith%40710@cluster0.bis5mkj.mongodb.net/Data?retryWrites=true&w=majority&appName=Cluster0"
-let db = "not connected"
+const DB =
+  "mongodb+srv://sujalchahande3:Smith%40710@cluster0.bis5mkj.mongodb.net/Data?retryWrites=true&w=majority&appName=Cluster0";
+let db = "not connected";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(DB);
-     db="connected"
+    db = "connected";
     console.log("Connected to MongoDB established");
   } catch (err) {
     console.log("Connection not established with mongoDB");
@@ -16,15 +18,21 @@ const connectDB = async () => {
 };
 connectDB();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000", 
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
+
 app.use("/api/candidate/", candidate);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome To StaffMerge" ,
-    atlas: {db}
-  });
+  res.json({ message: "Welcome To StaffMerge", atlas: { db } });
 });
 
-app.listen(3000, () => {
+app.listen(8000, () => {
   console.log("listening on port 3000");
 });
 
