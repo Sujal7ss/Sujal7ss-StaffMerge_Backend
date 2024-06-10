30c2367a7d3a22d8c629de10bec40fc56828476b
Sujal7ss <sujalchahande3@gmail.com>
2024-06-10 16:07:59 +0530

    backend

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import mongoose from "mongoose";
+// import candidate from "./routes/candidate.js"
+const app = express();
+
+const DB="mongodb+srv://sujalchahande3:Smith%40710@cluster0.bis5mkj.mongodb.net/Data?retryWrites=true&w=majority&appName=Cluster0"
+let db = "not connected"
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(DB);
+     db="connected"
+    console.log("Connected to MongoDB established");
+  } catch (err) {
+    console.log("Connection not established with mongoDB");
+  }
+};
+connectDB();
+
+// app.use("/api/candidate/", candidate);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome To StaffMerge" ,
+    atlas: {db}
+  });
+});
+
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+});
+
+export default app;
