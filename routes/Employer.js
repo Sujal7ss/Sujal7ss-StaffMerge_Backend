ee49669787d06553cc7e51b04bf13220e69f6d4d
Sujal7ss <sujalchahande3@gmail.com>
2024-06-10 23:12:57 +0530

    jobdetails

--- routes/Employer.js
+++ routes/Employer.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import {signup, login} from '../Controllers/Employer.js'
 import {companyDetails, companyData, companyDataUpdate} from '../Controllers/Company.js'
+import {jobDetails, postedJobs, editJob, selectCandidate} from "../Controllers/Job.js"
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/companyDetails", companyDetails);
-// router.get("/companyDetails", companyData)
-// router.post("/jobDetails", jobDetails);
+router.get("/companyDetails", companyData)
+router.post("/jobDetails", jobDetails);
 // router.post("/companyDetails", companyDataUpdate)
 // router.get("/postedJobs", postedJobs)
 
