73bac90b8b163c7871e7cc4aee0b72142ebe79be
Sujal Chahande <104003078+Sujal7ss@users.noreply.github.com>
2024-06-10 22:58:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import candidate from "./routes/candidate.js";
-// import employer from "./routes/employer.js";
+import employer from "./routes/Employer.js";
 
 import cors from "cors";
 import path from "path";
